83eea8eb4c5152106a1a7c0eee4630e65445b800
Colin Kiama <colinkiama@gmail.com>
2023-01-07 00:12:18 +0000

    Draw colour button circles
    They are horizontally aligned to the center but, all red for now.

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,30 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
+import { Application, Graphics } from "pixi.js";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let app = new Application({ width: 360, height: 640 });
+document.body.appendChild(app.view as HTMLCanvasElement);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const colorButtonGraphics = new Graphics();
+
+const COLOR_BUTTON_RADIUS = 25;
+const COLOR_BUTTON_SPACING = 10;
+
+for (let i = 0; i < 5; i++) {
+  const circleGeometry = {
+    x: (i % 5) * (COLOR_BUTTON_RADIUS * 2 + COLOR_BUTTON_SPACING),
+    y: 0,
+    radius: COLOR_BUTTON_RADIUS,
+  };
+
+  colorButtonGraphics
+    .beginFill(0xff0000)
+    .drawCircle(circleGeometry.x, circleGeometry.y, circleGeometry.radius)
+    .endFill();
+}
+
+colorButtonGraphics.x = app.screen.width / 2;
+colorButtonGraphics.y = app.screen.height - 80;
+colorButtonGraphics.pivot.x =
+  colorButtonGraphics.width / 2 - COLOR_BUTTON_RADIUS;
+
+app.stage.addChild(colorButtonGraphics);
