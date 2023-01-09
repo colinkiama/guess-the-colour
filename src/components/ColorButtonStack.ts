f845346d3e49ab62738df874306ddbb4388041ea
Colin Kiama <colinkiama@gmail.com>
2023-01-09 23:22:13 +0000

    Start handling player's chosen colour
    Player's colour choices are loogged in their browser's console

--- src/components/ColorButtonStack.ts
+++ src/components/ColorButtonStack.ts
@@ -1,4 +1,9 @@
-import { Application, Graphics } from "pixi.js";
+import {
+  Application,
+  Container,
+  FederatedPointerEvent,
+  Graphics,
+} from "pixi.js";
 import { Colors } from "../consts/Colors";
 import { Component } from "./Component";
 
@@ -21,27 +26,67 @@ export default class ColorButtonStack extends Component {
   }
 
   render() {
-    const colorButtonGraphics = new Graphics();
+    const colorButtonContainer = new Container<Graphics>();
 
     for (let i = 0; i < 5; i++) {
+      let colorButtonGraphics = new Graphics();
+
       const circleGeometry = {
         x: (i % 5) * (COLOR_BUTTON_RADIUS * 2 + COLOR_BUTTON_SPACING),
         y: 0,
         radius: COLOR_BUTTON_RADIUS,
       };
 
+      const buttonColor = COLOR_BUTTON_COLORS[i];
+
       colorButtonGraphics
-        .beginFill(COLOR_BUTTON_COLORS[i])
+        .beginFill(buttonColor)
         .drawCircle(circleGeometry.x, circleGeometry.y, circleGeometry.radius)
         .endFill();
+
+      colorButtonGraphics.interactive = true;
+      colorButtonGraphics.on("pointerdown", (evnt) =>
+        this.handleColorSelection(evnt, buttonColor)
+      );
+
+      colorButtonContainer.addChild(colorButtonGraphics);
     }
 
-    colorButtonGraphics.x = this.app.screen.width / 2;
-    colorButtonGraphics.y = this.app.screen.height - 80;
-    colorButtonGraphics.pivot.x =
-      colorButtonGraphics.width / 2 - COLOR_BUTTON_RADIUS;
+    colorButtonContainer.x = this.app.screen.width / 2;
+    colorButtonContainer.y = this.app.screen.height - 80;
+    colorButtonContainer.pivot.x =
+      colorButtonContainer.width / 2 - COLOR_BUTTON_RADIUS;
 
-    this.app.stage.addChild(colorButtonGraphics);
+    this.app.stage.addChild(colorButtonContainer);
+  }
+
+  handleColorSelection(
+    evnt: FederatedPointerEvent,
+    selectedColor: number
+  ): void {
+    // console.log(target.geometry.colors);
+
+    switch (selectedColor) {
+      case Colors.RED:
+        console.log("Player clicked on Red Button");
+        break;
+      case Colors.BLUE:
+        console.log("Player clicked on Blue Button");
+        break;
+      case Colors.GREEN:
+        console.log("Player clicked on Green Button");
+        break;
+      case Colors.YELLOW:
+        console.log("Player clicked on Yellow Button");
+        break;
+      case Colors.ORANGE:
+        console.log("Player clicked on Orange Button");
+        break;
+      default:
+        console.log("Couldn't handle color selection");
+        console.log("");
+        break;
+    }
   }
 
   destroy(): void {
